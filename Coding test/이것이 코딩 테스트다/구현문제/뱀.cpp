80c1a0d8b9b3a7db012edfe8c081f7737edfbc72
JINHYEON KIM <78076248+Jaykim1234@users.noreply.github.com>
2022-08-06 14:50:39 +0200

    Update 뱀.cpp

--- Coding test/이것이 코딩 테스트다/구현문제/뱀.cpp
+++ Coding test/이것이 코딩 테스트다/구현문제/뱀.cpp
@@ -110,44 +110,104 @@
 // 	return 0;
 // }
 
-#include <iostream>
-#include <vector>
-#include <queue>
+// #include <iostream>
+// #include <vector>
+// #include <queue>
 
-#define X _FirstImpl
-#define Y _SecondImpl
+// #define X _FirstImpl
+// #define Y _SecondImpl
 
+// using namespace std;
+
+// int dx[4] = {1, -1, 0 , 0};
+// int dy[4] = {0, 0, 1, -1};
+
+// int N, K, L;
+
+// int map[100][100];;
+// char direction[10001];
+
+// queue <pair<int, int>> q;
+
+// #include <bits/stdc++.h>  
+// using namespace test;  
+// int main()  
+// {  
+//   //declaring int. value in pair  
+//   vector< pair <int,int> > vec;  
+//   //defining two array value  
+//   int ary[] = {15, 20,10,30};  
+//   int ary1[] = 20, 40, 25, 60};  
+//   int num = sizeof(arr)/sizeof(ary[0]);  
+//   // enter any number 
+//   for (int x=0; x<num; x++)  
+//   vec.push_back( make_pair(ary[x],ary1[x]) );  
+//   // Publish vector value  
+//   for (int x=0; x<num; x++)  
+//   {  
+//   // first and second value specify with sorting 
+//   cout << vec[x].first << " " 
+//   << vec[x].second << endl;  
+//   }  
+//   return 0;  
+// } 
+
+// #include <iostream>
+// #include <vector>
+// #include <queue>
+
+// using namespace std;
+
+// /* 변수 선언 */
+// struct Pos // 위치 정보 자료형
+// {
+//     int y, x;
+// };
+// struct Dir // 방향 전환 정보 자료형
+// {
+//     int t;
+//     char d;
+// };
+// struct Joint // 뱀의 꺾인 부분 자료형
+// {
+//     int y, x, d;
+// };
+
+#include <bits/stdc++.h>
 using namespace std;
+using pii = pair<int,int>;
+int n, m;
+vector <int> v[101];
+int bfs(){
+    int visited[101];
+    memset(visited,0,sizeof(visited));
+    int ans = 0x3f3f3f3f;
+    queue <pii> q;
+    q.push({1,0}); //현재 위치, 주사위 던진 횟수
+    
+    visited[1] = 1;
+    while(!q.empty()){
+        int x = q.front().first;
+        int moved = q.front().second;
+        if(x == 100) {ans = min(ans, moved); break;}
+        q.pop();
+        for(int i = 1; i <= 6; i++){
+            int nx = x + i;
+            if(nx > 100 || visited[nx]) continue;
+            visited[nx] = 1;
+            if(!v[nx].size()) q.push({nx,moved + 1});
+            else q.push({v[nx][0],moved + 1});
+        }
+    }
+    return ans;
+}
 
-int dx[4] = {1, -1, 0 , 0};
-int dy[4] = {0, 0, 1, -1};
-
-int N, K, L;
-
-int map[100][100];;
-char direction[10001];
-
-queue <pair<int, int>> q;
-
-#include <bits/stdc++.h>  
-using namespace test;  
-int main()  
-{  
-  //declaring int. value in pair  
-  vector< pair <int,int> > vec;  
-  //defining two array value  
-  int ary[] = {15, 20,10,30};  
-  int ary1[] = 20, 40, 25, 60};  
-  int num = sizeof(arr)/sizeof(ary[0]);  
-  // enter any number 
-  for (int x=0; x<num; x++)  
-  vec.push_back( make_pair(ary[x],ary1[x]) );  
-  // Publish vector value  
-  for (int x=0; x<num; x++)  
-  {  
-  // first and second value specify with sorting 
-  cout << vec[x].first << " " 
-  << vec[x].second << endl;  
-  }  
-  return 0;  
-} 
+int main(){
+    cin >> n >> m;
+    for(int i = 0; i < n + m; i++){
+        int start,end;
+        cin >> start >> end;
+        v[start].push_back(end);
+    }
+    cout << bfs() << '\n';
+}
